4102fdd65e7c4fe65f65cdfba3c274065f7e6054
Fabio <fabiodwyer@hotmail.com>
2020-02-27 12:38:50 -0500

    Updates

---
+++ precache-manifest.1e7d19fc85e7592cc082b0e8f91ae2e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86d1d07e8104e4622a025f4fa37cef7f",
+    "url": "/project5/index.html"
+  },
+  {
+    "revision": "073cd655ee04d9541889",
+    "url": "/project5/static/css/main.da867e01.chunk.css"
+  },
+  {
+    "revision": "eac7bd7cf47244ec5d5c",
+    "url": "/project5/static/js/2.a98cd175.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/project5/static/js/2.a98cd175.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "073cd655ee04d9541889",
+    "url": "/project5/static/js/main.9d7480f4.chunk.js"
+  },
+  {
+    "revision": "873bdfac643491134ea5",
+    "url": "/project5/static/js/runtime-main.4dd11a09.js"
+  },
+  {
+    "revision": "ddccd0100ce6adb52f1d5bb55fcf61ea",
+    "url": "/project5/static/media/saxPlayer.ddccd010.jpg"
+  }
+]);
